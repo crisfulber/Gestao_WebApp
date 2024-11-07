bdf23d642077f314635d10e2b07d1cfcad9ea73f
Cristiano Fülber <crisfulber@outlook.com>
2024-11-06 22:51:09 -0300

    feat: fix problems

--- login-page/src/app/pages/login/login.component.ts
+++ login-page/src/app/pages/login/login.component.ts
@@ -4,40 +4,46 @@ import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } f
 import { PrimaryImputComponent } from "../../components/primary-imput/primary-imput.component";
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
-
-interface loginForm{
+interface LoginForm {
   user: FormControl,
   password: FormControl
 }
 
-
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, LoginComponent, PrimaryImputComponent],
-  providers: [LoginService],
+  imports: [   
+    DefaultLoginLayoutComponent, 
+    ReactiveFormsModule,
+    PrimaryImputComponent
+
+  ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm!: FormGroup;
-  toastService: any;
+  loginForm!: FormGroup<LoginForm>;
 
   constructor(
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private toastService: ToastrService
   ){
     this.loginForm = new FormGroup({
-      user: new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required, Validators.minLength(6)])
+      user: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
   }
 
   submit(){
     this.loginService.login(this.loginForm.value.user, this.loginForm.value.password).subscribe({
-      next: () => this.toastService.success("Login realizado com sucesso!"),
-      error: () => this.toastService.error("Erro inesperado. Contate o administrador.")
+      next: () => this.toastService.success("Login feito com sucesso!"),
+      error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
     })
   }
 
